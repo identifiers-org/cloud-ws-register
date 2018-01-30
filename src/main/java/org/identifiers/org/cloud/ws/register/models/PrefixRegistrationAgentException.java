ba43e46a707b04dec07313528a97ea9e258d9af8
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 23:11:33 +0000

    PrefixRegistrationAgentException

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationAgentException.java
@@ -0,0 +1,14 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-01-30 23:11
+ * ---
+ */
+public class PrefixRegistrationAgentException extends RuntimeException {
+    public PrefixRegistrationAgentException(String message) {
+        super(message);
+    }
+}
