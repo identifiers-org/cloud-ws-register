f1f835545ee1e3ae3976747c62571c4e5552b290
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 17:07:12 +0000

    the model is a component

--- src/main/java/org/identifiers/org/cloud/ws/register/models/RegisterApiModel.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/RegisterApiModel.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import org.springframework.stereotype.Component;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -7,5 +9,7 @@ package org.identifiers.org.cloud.ws.register.models;
  * Timestamp: 2018-01-30 17:03
  * ---
  */
+
+@Component
 public class RegisterApiModel {
 }
