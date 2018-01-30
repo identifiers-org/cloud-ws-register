8e1610fca89d67bd41b9f803f06dc46c370a022c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 17:13:10 +0000

    ignore httpStatus property when serializing the object

--- src/main/java/org/identifiers/org/cloud/ws/register/models/RegisterApiResponse.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/RegisterApiResponse.java
@@ -1,5 +1,6 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.http.HttpStatus;
 
 import java.io.Serializable;
@@ -11,6 +12,7 @@ import java.io.Serializable;
  * Timestamp: 2018-01-30 17:11
  * ---
  */
+@JsonIgnoreProperties(value = {"httpStatus"})
 public class RegisterApiResponse implements Serializable {
     private HttpStatus httpStatus = HttpStatus.OK;
 }
