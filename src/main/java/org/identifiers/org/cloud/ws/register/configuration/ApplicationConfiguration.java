37bf778803f2717a981e8d022e50b79cb57bd611
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 22:38:45 +0000

    application production configuration

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/configuration/ApplicationConfiguration.java
@@ -0,0 +1,14 @@
+package org.identifiers.org.cloud.ws.register.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.configuration
+ * Timestamp: 2018-01-30 22:37
+ * ---
+ */
+@Configuration
+public class ApplicationConfiguration {
+}
