c48591b5385b923f4b9b4734ad6a45c62b8fd21a
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 21:13:13 +0000

    PrefixRegistrationRequestValidatorException

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorException.java
@@ -0,0 +1,14 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-01-30 21:12
+ * ---
+ */
+public class PrefixRegistrationRequestValidatorException extends RuntimeException {
+    public PrefixRegistrationRequestValidatorException(String message) {
+        super(message);
+    }
+}
