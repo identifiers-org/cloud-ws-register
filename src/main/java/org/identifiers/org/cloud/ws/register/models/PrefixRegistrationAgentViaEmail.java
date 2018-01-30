d578acb0cdde8be1cd7d1a7a3071c28fa4afe830
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 23:13:59 +0000

    PrefixRegistrationAgentViaEmail agent placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationAgentViaEmail.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-01-30 23:13
+ * ---
+ */
+public class PrefixRegistrationAgentViaEmail implements PrefixRegistrationAgent {
+    @Override
+    public void registerPrefix(RegisterApiRequestRegisterPrefix prefixRegistrationRequest) throws PrefixRegistrationAgentException {
+        // TODO
+    }
+}
