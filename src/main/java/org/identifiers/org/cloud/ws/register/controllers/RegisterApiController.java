60d548e0aca534db8cafab2ea802f7c9389891e5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 17:04:14 +0000

    annotation for rest controller

--- src/main/java/org/identifiers/org/cloud/ws/register/controllers/RegisterApiController.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/controllers/RegisterApiController.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.register.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -7,5 +9,6 @@ package org.identifiers.org.cloud.ws.register.controllers;
  * Timestamp: 2018-01-30 17:02
  * ---
  */
+@RestController
 public class RegisterApiController {
 }
