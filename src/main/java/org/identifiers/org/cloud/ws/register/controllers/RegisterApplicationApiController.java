658a987aa1157eba619ec7503b2b2ac3e57f45da
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 17:02:22 +0000

    api controller placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/controllers/RegisterApplicationApiController.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.register.controllers;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.controllers
+ * Timestamp: 2018-01-30 17:02
+ * ---
+ */
+public class RegisterApplicationApiController {
+}
