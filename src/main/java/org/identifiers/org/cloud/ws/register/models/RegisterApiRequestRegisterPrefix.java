7d11985180d6fecdc4f31740829b0a16fb422cb8
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 20:55:55 +0000

    getters and setters

--- src/main/java/org/identifiers/org/cloud/ws/register/models/RegisterApiRequestRegisterPrefix.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/RegisterApiRequestRegisterPrefix.java
@@ -23,4 +23,84 @@ public class RegisterApiRequestRegisterPrefix implements Serializable {
     private String regexPattern;
     private String[] references;
     private String additionalInformation;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public void setHomePage(String homePage) {
+        this.homePage = homePage;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    public String getPreferredPrefix() {
+        return preferredPrefix;
+    }
+
+    public void setPreferredPrefix(String preferredPrefix) {
+        this.preferredPrefix = preferredPrefix;
+    }
+
+    public String getResourceAccessRule() {
+        return resourceAccessRule;
+    }
+
+    public void setResourceAccessRule(String resourceAccessRule) {
+        this.resourceAccessRule = resourceAccessRule;
+    }
+
+    public String getExampleIdentifier() {
+        return exampleIdentifier;
+    }
+
+    public void setExampleIdentifier(String exampleIdentifier) {
+        this.exampleIdentifier = exampleIdentifier;
+    }
+
+    public String getRegexPattern() {
+        return regexPattern;
+    }
+
+    public void setRegexPattern(String regexPattern) {
+        this.regexPattern = regexPattern;
+    }
+
+    public String[] getReferences() {
+        return references;
+    }
+
+    public void setReferences(String[] references) {
+        this.references = references;
+    }
+
+    public String getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    public void setAdditionalInformation(String additionalInformation) {
+        this.additionalInformation = additionalInformation;
+    }
 }
