bde615047798d774a6f019bfae37ab4adde0c2c7
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 20:54:08 +0000

    is serializable

--- src/main/java/org/identifiers/org/cloud/ws/register/models/Requester.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/Requester.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import java.io.Serializable;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -7,5 +9,5 @@ package org.identifiers.org.cloud.ws.register.models;
  * Timestamp: 2018-01-30 20:53
  * ---
  */
-public class Requester {
+public class Requester implements Serializable {
 }
