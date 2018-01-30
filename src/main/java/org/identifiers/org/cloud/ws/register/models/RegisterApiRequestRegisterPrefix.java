f664a554c2ce064a33ade616ca5df4a49d674cad
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 17:48:51 +0000

    register api request object, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/RegisterApiRequestRegisterPrefix.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-01-30 17:47
+ * ---
+ */
+public class RegisterApiRequestRegisterPrefix {
+}
