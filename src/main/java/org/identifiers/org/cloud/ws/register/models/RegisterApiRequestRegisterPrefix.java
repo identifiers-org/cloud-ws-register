cb123724f9a81e6b1480c22cabe3e64d8629b7d7
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 18:02:48 +0000

    register request model, preferred prefix

--- src/main/java/org/identifiers/org/cloud/ws/register/models/RegisterApiRequestRegisterPrefix.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/RegisterApiRequestRegisterPrefix.java
@@ -17,4 +17,5 @@ public class RegisterApiRequestRegisterPrefix implements Serializable {
     private String description;
     private String homePage;
     private String organization;
+    private String preferredPrefix;
 }
