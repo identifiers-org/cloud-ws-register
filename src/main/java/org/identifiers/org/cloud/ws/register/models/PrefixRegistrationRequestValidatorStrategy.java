a849761a04573ab8ea7fc1ec7931aaea77c34a98
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-30 22:15:15 +0000

    PrefixRegistrationRequestValidatorStrategy placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorStrategy.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-01-30 22:14
+ * ---
+ */
+public interface PrefixRegistrationRequestValidatorStrategy extends PrefixRegistrationRequestValidator {
+}
