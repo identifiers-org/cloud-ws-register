f5554788317ec20134f74266b8990e902d4819f3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-31 00:24:07 +0000

    annotate controller parameter as request body

--- src/main/java/org/identifiers/org/cloud/ws/register/controllers/RegisterApiController.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/controllers/RegisterApiController.java
@@ -5,6 +5,7 @@ import org.identifiers.org.cloud.ws.register.models.RegisterApiRequestRegisterPr
 import org.identifiers.org.cloud.ws.register.models.RegisterApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +24,7 @@ public class RegisterApiController {
     private RegisterApiModel registerApiModel;
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public ResponseEntity<?> registerPrefix(RegisterApiRequestRegisterPrefix registerApiRequestRegisterPrefix) {
+    public ResponseEntity<?> registerPrefix(@RequestBody RegisterApiRequestRegisterPrefix registerApiRequestRegisterPrefix) {
         // The model associated with the controller should handle any possible exception that could happen while running
         // the business logic, thus, the controller should handle only exceptions within the domain of the controller.
         RegisterApiResponse registerApiResponse = registerApiModel.registerPrefix(registerApiRequestRegisterPrefix);
