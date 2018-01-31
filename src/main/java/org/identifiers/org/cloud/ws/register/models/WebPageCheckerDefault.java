ffffbc4f6403d8b5b13dc9118f44287236a1cca7
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-31 12:33:37 +0000

    default web checker, check for valid URL

--- src/main/java/org/identifiers/org/cloud/ws/register/models/WebPageCheckerDefault.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/WebPageCheckerDefault.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import org.apache.commons.validator.routines.UrlValidator;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -11,6 +13,10 @@ public class WebPageCheckerDefault implements WebPageChecker {
     @Override
     public boolean checkWebPageUrl(String webPageUrl) throws WebPageCheckerException {
         // TODO
+        // Check for invalid URL
+        if (!(new UrlValidator()).isValid(webPageUrl)) {
+            throw new WebPageCheckerException(String.format("Home Page URL '%s' is NOT VALID", webPageUrl));
+        }
         return true;
     }
 }
