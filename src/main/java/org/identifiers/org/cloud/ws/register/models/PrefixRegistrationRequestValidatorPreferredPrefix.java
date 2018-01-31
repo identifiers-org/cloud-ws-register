5b9c0cbd3cf1af98acb4adb3a37d9319e1dc6cd3
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-31 14:40:48 +0000

    preferred prefix validator, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorPreferredPrefix.java
@@ -0,0 +1,18 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-01-31 14:40
+ * ---
+ */
+public class PrefixRegistrationRequestValidatorPreferredPrefix implements PrefixRegistrationRequestValidator {
+    @Override
+    public boolean validate(RegisterApiRequestRegisterPrefix request) throws PrefixRegistrationRequestValidatorException {
+        return false;
+    }
+}
