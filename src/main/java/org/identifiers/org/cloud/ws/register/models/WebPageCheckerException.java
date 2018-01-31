fe48331dafe6a0069e71c8a1dff4aaf76cd1e2cd
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-31 12:25:54 +0000

    WebPageCheckerException implementation

--- src/main/java/org/identifiers/org/cloud/ws/register/models/WebPageCheckerException.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/WebPageCheckerException.java
@@ -7,5 +7,8 @@ package org.identifiers.org.cloud.ws.register.models;
  * Timestamp: 2018-01-31 12:25
  * ---
  */
-public class WebPageCheckerException {
+public class WebPageCheckerException extends RuntimeException {
+    public WebPageCheckerException(String message) {
+        super(message);
+    }
 }
