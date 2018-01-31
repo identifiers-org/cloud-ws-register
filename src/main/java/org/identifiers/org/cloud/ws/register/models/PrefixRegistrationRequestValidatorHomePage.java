6473f5d5f8dcc8c13ec9c4a83a5668645324100d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-31 12:49:21 +0000

    refactor

--- src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorHomePage.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorHomePage.java
@@ -1,12 +1,6 @@
 package org.identifiers.org.cloud.ws.register.models;
 
-import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -27,30 +21,12 @@ public class PrefixRegistrationRequestValidatorHomePage implements PrefixRegistr
         if (request.getHomePage() == null) {
             throw new PrefixRegistrationRequestValidatorException("MISSING URL describing the resource");
         }
-        // Check for invalid URL
-        if (!(new UrlValidator()).isValid(request.getHomePage())) {
-            throw new PrefixRegistrationRequestValidatorException(String.format("Home Page URL '%s' is NOT VALID", request.getHomePage()));
-        }
-        // Detect Dead home page by accessing it and making sure we get an HTTP 200 OK, in the future, more complex
-        // checkers can be externalized, and pack as dead home page detection strategies
-        int status = 0;
+        boolean valid = true;
         try {
-            // TODO - Add a re-try policy for when we try to connect the URL
-            URL testUrl = new URL(request.getHomePage());
-            HttpURLConnection connection = (HttpURLConnection) testUrl.openConnection();
-            connection.setRequestMethod("GET");
-            // TODO - Refactor this out as constants
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
-            status = connection.getResponseCode();
-            connection.disconnect();
-        } catch (IOException e) {
-            // Including MalformedURLException
-            throw new PrefixRegistrationRequestValidatorException(String.format("When checking your home page, '%s', the following error occurred '%s'", request.getHomePage(), e.getMessage()));
+            valid = webPageChecker.checkWebPageUrl(request.getHomePage());
+        } catch (WebPageCheckerException e) {
+            throw new PrefixRegistrationRequestValidatorException(e.getMessage());
         }
-        if (HttpStatus.valueOf(status) != HttpStatus.OK) {
-            throw new PrefixRegistrationRequestValidatorException(String.format("When checking your home page at '%s', we got the following HTTP Status Code %d", request.getHomePage(), status));
-        }
-        return true;
+        return valid;
     }
 }
