e2081aa49515a639595e7616ebe3d53601cac51b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-01-31 12:25:40 +0000

    WebPageCheckerException placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/WebPageCheckerException.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-01-31 12:25
+ * ---
+ */
+public class WebPageCheckerException {
+}
