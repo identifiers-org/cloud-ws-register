e6833c883c63647e726e3fbbcd89834d30643188
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-01 13:44:11 +0000

    refactoring of web page checker

--- src/main/java/org/identifiers/org/cloud/ws/register/models/WebPageCheckerDefault.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/WebPageCheckerDefault.java
@@ -1,12 +1,5 @@
 package org.identifiers.org.cloud.ws.register.models;
 
-import org.apache.commons.validator.routines.UrlValidator;
-import org.springframework.http.HttpStatus;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -15,42 +8,5 @@ import java.net.URL;
  * ---
  */
 public class WebPageCheckerDefault implements WebPageChecker {
-    // By breaking different checking steps into externalized pieces, I can reuse this code on other possible new web
-    // page checkers
-    public static boolean checkForValidUrl(String url) {
-        return (new UrlValidator()).isValid(url);
-    }
-
-    public static boolean checkForOkUrlEndpoint(String url) throws WebPageCheckerException {
-        // Detect Dead endpoint by accessing it and making sure we get an HTTP 200 OK, in the future, more complex
-        // checkers can be externalized, and pack as dead URL Endpoint detection strategies
-        int status = 0;
-        try {
-            // TODO - Add a re-try policy for when we try to connect the URL
-            URL testUrl = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) testUrl.openConnection();
-            connection.setRequestMethod("GET");
-            // TODO - Refactor this out as constants
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(5000);
-            status = connection.getResponseCode();
-            connection.disconnect();
-        } catch (IOException e) {
-            // Including MalformedURLException
-            throw new WebPageCheckerException(String.format("When checking your URL Endpoint, '%s', the following error occurred '%s'", url, e.getMessage()));
-        }
-        if (HttpStatus.valueOf(status) != HttpStatus.OK) {
-            throw new WebPageCheckerException(String.format("When checking your URL Endpoint at '%s', we got the following HTTP Status Code %d", url, status));
-        }
-        return true;
-    }
-
-    @Override
-    public boolean checkWebPageUrl(String webPageUrl) throws WebPageCheckerException {
-        // Check for invalid URL
-        if (!checkForValidUrl(webPageUrl)) {
-            throw new WebPageCheckerException(String.format("URL '%s' is NOT VALID", webPageUrl));
-        }
-        return checkForOkUrlEndpoint(webPageUrl);
-    }
+    // Default web page checker doesn't do anything extraordinary
 }
