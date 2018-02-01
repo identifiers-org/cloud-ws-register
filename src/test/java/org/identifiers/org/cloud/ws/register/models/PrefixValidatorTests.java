0593bc5eee51fb206ecdfadb3d44204868029ef7
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-01 09:49:07 +0000

    It may not be parameterized

--- src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
@@ -1,8 +1,5 @@
 package org.identifiers.org.cloud.ws.register.models;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -10,6 +7,5 @@ import org.junit.runners.Parameterized;
  * Timestamp: 2018-02-01 9:28
  * ---
  */
-@RunWith(Parameterized.class)
 public class PrefixValidatorTests {
 }
