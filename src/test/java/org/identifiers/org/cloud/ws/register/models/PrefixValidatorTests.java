e47196337fe28e0d0380f11c37b6e82b6788a455
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-01 10:00:44 +0000

    so far so long... everything seems to be getting in, don't know if it works yet

--- src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
@@ -5,6 +5,9 @@ import org.junit.runners.Parameterized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -25,5 +28,14 @@ public class PrefixValidatorTests {
     public PrefixValidatorTests(RegisterApiRequestRegisterPrefix request, String testDescription) {
         this.request = request;
         this.testDescription = testDescription;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getTestingValues() {
+        // TODO
+        return Arrays.asList(new Object[][]{
+                // Test name validator
+                {new RegisterApiRequestRegisterPrefix().setPreferredPrefix("myfirstprefix"), "This request has an invalid name, it is null"}
+        });
     }
 }
