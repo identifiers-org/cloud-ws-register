a2967f5aa3679b1c86b0c4f09a4c029d942ac3dc
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-01 09:53:48 +0000

    still not recognized

--- src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
@@ -1,6 +1,8 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
  * Timestamp: 2018-02-01 9:28
  * ---
  */
+@RunWith(SpringJUnit4ClassRunner.class)
 public class PrefixValidatorTests {
 
     @Autowired
