95383bee1b5905cc59c4dd5dac939b326851b48f
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-01 13:37:44 +0000

    PLACEHOLDER_ID constant

--- src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorResourceAccessRule.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorResourceAccessRule.java
@@ -8,6 +8,8 @@ package org.identifiers.org.cloud.ws.register.models;
  * ---
  */
 public class PrefixRegistrationRequestValidatorResourceAccessRule implements PrefixRegistrationRequestValidator {
+    private static String PLACEHOLDER_ID = "{$id}";
+
     @Override
     public boolean validate(RegisterApiRequestRegisterPrefix request) throws PrefixRegistrationRequestValidatorException {
         return false;
