c8096f9d063d3ab0afccd6ba1cb3c3725a5054ad
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-01 09:33:40 +0000

    this unit test will be parameterized

--- src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -7,5 +10,6 @@ package org.identifiers.org.cloud.ws.register.models;
  * Timestamp: 2018-02-01 9:28
  * ---
  */
+@RunWith(Parameterized.class)
 public class PrefixValidatorTests {
 }
