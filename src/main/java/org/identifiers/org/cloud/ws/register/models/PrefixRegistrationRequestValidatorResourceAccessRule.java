9d2290b66074c8110c38b2fba79ad8d2e6b33bc1
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-01 12:22:01 +0000

    validator for resource access rule, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorResourceAccessRule.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-01 12:21
+ * ---
+ */
+public class PrefixRegistrationRequestValidatorResourceAccessRule implements PrefixRegistrationRequestValidator {
+    @Override
+    public boolean validate(RegisterApiRequestRegisterPrefix request) throws PrefixRegistrationRequestValidatorException {
+        return false;
+    }
+}
