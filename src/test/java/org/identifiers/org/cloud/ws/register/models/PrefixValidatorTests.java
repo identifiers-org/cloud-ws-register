3741802873b73b09e1496645e0c5afdfba71153e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-01 09:52:23 +0000

    autowire prefix validator, but it doesn't autowire yet

--- src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -9,4 +11,6 @@ package org.identifiers.org.cloud.ws.register.models;
  */
 public class PrefixValidatorTests {
 
+    @Autowired
+    private PrefixRegistrationRequestValidatorPreferredPrefix prefixValidator;
 }
