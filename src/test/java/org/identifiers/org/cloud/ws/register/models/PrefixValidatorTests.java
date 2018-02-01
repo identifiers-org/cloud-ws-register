464a688d0138644d3a2d201e69eb835582152377
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-01 10:10:51 +0000

    ouch, typo there

--- src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
@@ -6,6 +6,7 @@ import org.junit.runners.Parameterized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -22,7 +23,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
  */
 @SpringBootTest
 @SpringBootConfiguration
-@RunWith(Parameterized.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class PrefixValidatorTests {
 
     @Autowired
