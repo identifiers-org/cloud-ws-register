66b22b588f7b65c8641ec0de78b11a6b3fd3bb29
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-01 09:29:02 +0000

    prefix validator tests, placeholder

---
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixValidatorTests.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-01 9:28
+ * ---
+ */
+public class PrefixValidatorTests {
+}
