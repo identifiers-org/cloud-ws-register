d2fb6544e5175b36800eb866874e6ee2bad458df
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 14:34:41 +0000

    requester validator factory, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/RequesterValidatorFactory.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-02 14:34
+ * ---
+ */
+public class RequesterValidatorFactory {
+}
