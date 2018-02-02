3a2144236b6e2340f11549b77fe574871e3fc632
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 12:41:43 +0000

    validator for references, logger

--- src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorReferences.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorReferences.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -8,6 +11,8 @@ package org.identifiers.org.cloud.ws.register.models;
  * ---
  */
 public class PrefixRegistrationRequestValidatorReferences implements PrefixRegistrationRequestValidator {
+    private static Logger logger = LoggerFactory.getLogger(PrefixRegistrationRequestValidatorReferences.class);
+    
     @Override
     public boolean validate(RegisterApiRequestRegisterPrefix request) throws PrefixRegistrationRequestValidatorException {
         // References are not enforced, so they just validate
