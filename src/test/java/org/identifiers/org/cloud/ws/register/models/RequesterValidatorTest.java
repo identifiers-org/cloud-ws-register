76f5c98088f0680aeedc766d9e8cfc4a4382487c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 13:44:11 +0000

    requester validator unit tests, runner

--- src/test/java/org/identifiers/org/cloud/ws/register/models/RequesterValidatorTest.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/RequesterValidatorTest.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -7,6 +10,7 @@ package org.identifiers.org.cloud.ws.register.models;
  * Timestamp: 2018-02-02 13:41
  * ---
  */
+@RunWith(SpringJUnit4ClassRunner.class)
 public class RequesterValidatorTest {
 
 }
