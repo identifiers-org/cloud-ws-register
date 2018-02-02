4601c465bb8041368eb147a0513748a5bd84353e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 14:42:15 +0000

    data for invalid unit test case for requester validator

--- src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorNotValidCasesTests.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorNotValidCasesTests.java
@@ -64,7 +64,8 @@ public class PrefixRegistrationRequestValidatorNotValidCasesTests {
                 {new PrefixRegistrationRequestValidatorCrossedRegexPatternAndExampleIdentifier(), new RegisterApiRequestRegisterPrefix(), "NOT Valid cross-validation of example identifier and regular expression pattern describing the identifier, missing regex pattern and example identifier"},
                 {new PrefixRegistrationRequestValidatorCrossedRegexPatternAndExampleIdentifier(), new RegisterApiRequestRegisterPrefix().setRegexPattern("\\d+$"), "NOT Valid cross-validation of example identifier and regular expression pattern describing the identifier, present regex pattern but missing example identifier"},
                 {new PrefixRegistrationRequestValidatorCrossedRegexPatternAndExampleIdentifier(), new RegisterApiRequestRegisterPrefix().setExampleIdentifier("ENSX00000139618"), "NOT Valid cross-validation of example identifier and regular expression pattern describing the identifier, present example identifier but missing regex pattern"},
-                {new PrefixRegistrationRequestValidatorCrossedRegexPatternAndExampleIdentifier(), new RegisterApiRequestRegisterPrefix().setRegexPattern("\\d+$").setExampleIdentifier("ENSX00000139618"), "NOT Valid cross-validation of example identifier and regular expression pattern describing the identifier"}
+                {new PrefixRegistrationRequestValidatorCrossedRegexPatternAndExampleIdentifier(), new RegisterApiRequestRegisterPrefix().setRegexPattern("\\d+$").setExampleIdentifier("ENSX00000139618"), "NOT Valid cross-validation of example identifier and regular expression pattern describing the identifier"},
+                {new PrefixRegistrationRequestValidatorRequester(), new RegisterApiRequestRegisterPrefix().setRequester(new Requester().setEmail("invalidATemail.com")),"Provisioned requester details DOES NOT VALIDATE"}
         });
     }
 
