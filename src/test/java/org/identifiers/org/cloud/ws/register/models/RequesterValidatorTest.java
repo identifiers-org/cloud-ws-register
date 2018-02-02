caa4e1b038eb082532b44ecb40ea1e1f45cb549b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 13:51:13 +0000

    requester validator unit tests, getNotValidTestCasesData() placeholder

--- src/test/java/org/identifiers/org/cloud/ws/register/models/RequesterValidatorTest.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/RequesterValidatorTest.java
@@ -33,4 +33,8 @@ public class RequesterValidatorTest {
     private List<TestDataUseCase> getValidTestDataUseCases() {
         // TODO
     }
+
+    private List<TestDataUseCase> getNotValidTestCasesData() {
+        // TODO
+    }
 }
