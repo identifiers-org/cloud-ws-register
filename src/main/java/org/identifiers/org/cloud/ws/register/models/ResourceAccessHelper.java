55194290e7c108c61aae10c74b28b19aaef7389d
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 10:04:32 +0000

    getResourceUrlFor() implementation

--- src/main/java/org/identifiers/org/cloud/ws/register/models/ResourceAccessHelper.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/ResourceAccessHelper.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -9,4 +11,8 @@ package org.identifiers.org.cloud.ws.register.models;
  */
 public class ResourceAccessHelper {
     public static final String RESOURCE_ACCESS_RULE_PLACEHOLDER_ID = "{$id}";
+
+    public static String getResourceUrlFor(@NotNull String accessRule, @NotNull String id) {
+        return accessRule.replace(RESOURCE_ACCESS_RULE_PLACEHOLDER_ID, id);
+    }
 }
