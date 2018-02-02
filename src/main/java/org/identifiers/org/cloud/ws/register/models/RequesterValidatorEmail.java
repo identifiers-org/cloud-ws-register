ee63c6ab7fc61317192034329f0274f8dd0964f2
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 14:01:52 +0000

    requester email validator, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/RequesterValidatorEmail.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-02 14:01
+ * ---
+ */
+public class RequesterValidatorEmail implements RequesterValidator {
+    @Override
+    public boolean validate(Requester requester) throws RequesterValidatorException {
+        return false;
+    }
+}
