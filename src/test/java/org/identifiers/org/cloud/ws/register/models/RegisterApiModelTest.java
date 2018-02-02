bd244f57addc7d61bf38f3993fca921b6040ef18
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 16:09:55 +0000

    register api model, unit test, placeholder

---
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/RegisterApiModelTest.java
@@ -0,0 +1,12 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-02 16:09
+ * ---
+ */
+public class RegisterApiModelTest {
+
+}
