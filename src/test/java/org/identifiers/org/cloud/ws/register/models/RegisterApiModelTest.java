dd569ccfeb1e8468265176bed4a9c9aeeacf169e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 16:13:49 +0000

    register api model, unit test, will run with spring runner and it's a spring boot test

--- src/test/java/org/identifiers/org/cloud/ws/register/models/RegisterApiModelTest.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/RegisterApiModelTest.java
@@ -1,5 +1,9 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -7,6 +11,8 @@ package org.identifiers.org.cloud.ws.register.models;
  * Timestamp: 2018-02-02 16:09
  * ---
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class RegisterApiModelTest {
 
 }
