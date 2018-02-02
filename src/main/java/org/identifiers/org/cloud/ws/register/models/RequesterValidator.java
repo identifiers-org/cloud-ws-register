fa6ef7397254c1934901f52c58e3f309ca0a1b1e
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 13:19:41 +0000

    requester validator interface, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/RequesterValidator.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-02 13:19
+ * ---
+ */
+public class RequesterValidator {
+}
