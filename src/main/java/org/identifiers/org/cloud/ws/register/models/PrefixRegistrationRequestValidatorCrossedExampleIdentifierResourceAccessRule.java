1491b98ef75b0ee219f3a65006dbe5f6bcf06cde
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 09:54:39 +0000

    prepare the errors to report to the user

--- src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorCrossedExampleIdentifierResourceAccessRule.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorCrossedExampleIdentifierResourceAccessRule.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -10,6 +13,8 @@ package org.identifiers.org.cloud.ws.register.models;
 public class PrefixRegistrationRequestValidatorCrossedExampleIdentifierResourceAccessRule implements PrefixRegistrationRequestValidator {
     @Override
     public boolean validate(RegisterApiRequestRegisterPrefix request) throws PrefixRegistrationRequestValidatorException {
-        return false;
+        List<String> errors = new ArrayList<>();
+        // TODO
+        return true;
     }
 }
