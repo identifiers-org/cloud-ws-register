d4de2e0faf2394417afd9378ed2c1731f1517f22
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 16:17:38 +0000

    register api model, unit test, rule for invalid use caess

--- src/test/java/org/identifiers/org/cloud/ws/register/models/RegisterApiModelTest.java
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/RegisterApiModelTest.java
@@ -1,5 +1,7 @@
 package org.identifiers.org.cloud.ws.register.models;
 
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -29,4 +31,8 @@ public class RegisterApiModelTest {
             return this;
         }
     }
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
 }
