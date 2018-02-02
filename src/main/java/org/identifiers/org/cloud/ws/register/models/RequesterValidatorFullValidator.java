b073cfeab1305ae1d1dc4cf0fc4f01191afa2553
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 14:20:29 +0000

    requester full validator, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/RequesterValidatorFullValidator.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-02 14:19
+ * ---
+ */
+public class RequesterValidatorFullValidator implements RequesterValidator {
+    @Override
+    public boolean validate(Requester requester) throws RequesterValidatorException {
+        return false;
+    }
+}
