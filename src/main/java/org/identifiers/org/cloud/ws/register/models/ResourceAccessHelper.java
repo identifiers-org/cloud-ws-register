cd715ed917f9bc42552ecb55a37cdc9551543795
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 10:00:40 +0000

    resource access helper, placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/ResourceAccessHelper.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-02 10:00
+ * ---
+ */
+public class ResourceAccessHelper {
+}
