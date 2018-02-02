c63fee6581077049b0453997083fa62e1869878b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-02 13:42:09 +0000

    requester validator unit tests

---
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/RequesterValidatorTest.java
@@ -0,0 +1,12 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-02 13:41
+ * ---
+ */
+public class RequesterValidatorTest {
+
+}
