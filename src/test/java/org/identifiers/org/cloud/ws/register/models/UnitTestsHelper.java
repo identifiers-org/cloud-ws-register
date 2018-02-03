6fce3b055a08288370f60de857548ec0fe9b2933
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-03 05:58:17 +0000

    unit tests helper

---
+++ src/test/java/org/identifiers/org/cloud/ws/register/models/UnitTestsHelper.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-03 5:58
+ * ---
+ */
+public class UnitTestsHelper {
+}
