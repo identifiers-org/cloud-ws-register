d1c3af637863ab47affe7884e0dddb547d59acff
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-03 15:15:18 +0000

    annotation for the 'development' configuration profile

--- src/main/java/org/identifiers/org/cloud/ws/register/configuration/ApplicationConfigurationDevelopment.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/configuration/ApplicationConfigurationDevelopment.java
@@ -1,5 +1,8 @@
 package org.identifiers.org.cloud.ws.register.configuration;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
  * Project: register
@@ -7,5 +10,7 @@ package org.identifiers.org.cloud.ws.register.configuration;
  * Timestamp: 2018-02-03 15:07
  * ---
  */
+@Configuration
+@Profile("development")
 public class ApplicationConfigurationDevelopment {
 }
