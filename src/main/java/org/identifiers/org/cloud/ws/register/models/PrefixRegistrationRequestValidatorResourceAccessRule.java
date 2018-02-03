7ff3d48b64c7d3e10e1cb717d2b84fbea55d7a6c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-03 06:37:27 +0000

    no important change here

--- src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorResourceAccessRule.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationRequestValidatorResourceAccessRule.java
@@ -16,11 +16,11 @@ public class PrefixRegistrationRequestValidatorResourceAccessRule implements Pre
         if (request.getResourceAccessRule() == null) {
             throw new PrefixRegistrationRequestValidatorException("MISSING required Resource Access Rule");
         }
-        WebPageChecker webPageChecker = WebPageCheckerFactory.getWebPageChecker();
         // Check that PLACEHOLDER_ID is uniquely present
         if (StringUtils.countOccurrencesOf(request.getResourceAccessRule(), ResourceAccessHelper.RESOURCE_ACCESS_RULE_PLACEHOLDER_ID) != 1) {
             throw new PrefixRegistrationRequestValidatorException(String.format("ID placeholder '%s' IS REQUIRED to be present at least once in the resource access rule", ResourceAccessHelper.RESOURCE_ACCESS_RULE_PLACEHOLDER_ID));
         }
+        WebPageChecker webPageChecker = WebPageCheckerFactory.getWebPageChecker();
         String urlToCheck = StringUtils.replace(request.getResourceAccessRule(), ResourceAccessHelper.RESOURCE_ACCESS_RULE_PLACEHOLDER_ID, "placeholderId");
         // Remove the PLACEHOLDER_ID from thr URL for standalone checking
         if (!webPageChecker.checkForValidUrl(urlToCheck)) {
