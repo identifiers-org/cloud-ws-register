107cc69f544b143ca71d952091b80d09e5db631b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-03 15:19:22 +0000

    PrefixRegistrationAgentDumpRequestIntoLogs placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/PrefixRegistrationAgentDumpRequestIntoLogs.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.register.models;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.models
+ * Timestamp: 2018-02-03 15:19
+ * ---
+ */
+public class PrefixRegistrationAgentDumpRequestIntoLogs implements PrefixRegistrationAgent {
+    @Override
+    public void registerPrefix(RegisterApiRequestRegisterPrefix prefixRegistrationRequest) throws PrefixRegistrationAgentException {
+
+    }
+}
