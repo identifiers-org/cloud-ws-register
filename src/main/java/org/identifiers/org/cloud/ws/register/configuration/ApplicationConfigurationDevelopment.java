b50045694d939711ae23ced69a6465229ed5811c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-02-03 15:08:02 +0000

    application configuration for development

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/configuration/ApplicationConfigurationDevelopment.java
@@ -0,0 +1,11 @@
+package org.identifiers.org.cloud.ws.register.configuration;
+
+/**
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.configuration
+ * Timestamp: 2018-02-03 15:07
+ * ---
+ */
+public class ApplicationConfigurationDevelopment {
+}
