2e718adbeba51467e1d36a242393385204cde261
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-03-08 09:15:05 +0000

    refactoring validator for being managed by Spring Boot

--- src/main/java/org/identifiers/org/cloud/ws/register/models/validators/PrefixRegistrationRequestValidatorDescription.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/models/validators/PrefixRegistrationRequestValidatorDescription.java
@@ -1,6 +1,9 @@
 package org.identifiers.org.cloud.ws.register.models.validators;
 
 import org.identifiers.org.cloud.ws.register.models.api.requests.prefixregistration.ServiceRequestRegisterPrefixPayload;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -9,6 +12,9 @@ import org.identifiers.org.cloud.ws.register.models.api.requests.prefixregistrat
  * Timestamp: 2018-01-31 10:26
  * ---
  */
+@Component
+@Scope("prototype")
+@Qualifier("PrefixRegistrationRequestValidatorDescription")
 public class PrefixRegistrationRequestValidatorDescription implements PrefixRegistrationRequestValidator {
     public static final int DESCRIPTION_CONTENT_MIN_CHARS = 50;
 
