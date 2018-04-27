51075b841475f809cf1a71114b74a41b0fe9b7f4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-04-27 13:52:45 +0100

    refactoring to introduce CORS origins

--- src/main/java/org/identifiers/org/cloud/ws/register/configuration/ApplicationConfiguration.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/configuration/ApplicationConfiguration.java
@@ -1,7 +1,12 @@
 package org.identifiers.org.cloud.ws.register.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author Manuel Bernal Llinares <mbdebian@gmail.com>
@@ -15,4 +20,17 @@ import org.springframework.context.annotation.Profile;
 public class ApplicationConfiguration {
     // Production Environment Application Configuration
 
+    @Value("${org.identifiers.cloud.ws.register.cors.origin}")
+    private String corsOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(corsOrigins.split(","));
+            }
+        };
+    }
 }
