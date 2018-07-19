3c1a83feed2cabb24d02a08bcfc5d4363c15a55b
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-07-19 12:59:11 +0100

    refactoring health API

--- src/main/java/org/identifiers/org/cloud/ws/register/api/controllers/RegisterApiController.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/api/controllers/RegisterApiController.java
@@ -31,23 +31,4 @@ public class RegisterApiController {
         return new ResponseEntity<>(serviceResponseRegisterPrefix, serviceResponseRegisterPrefix.getHttpStatus());
     }
 
-    // liveness probe
-    // TODO - This is being refactored out in the next iteration
-    @RequestMapping(value = "/liveness_check")
-    public String livenessCheck() {
-        // TODO - This will be refactored out later, it will be the model who will implement the logic to determine
-        // TODO - whether the service should be considered "alive" or not, but this code will live here for testing
-        // TODO - purposes
-        return registerApiModel.livenessCheck();
-    }
-
-    // Readiness check
-    @RequestMapping(value = "/readiness_check")
-    public String readinessCheck() {
-        // TODO - This will be refactored out later, it will be the model who will implement the logic to determine
-        // TODO - whether the service should be considered "ready" or not, but this code will live here for testing
-        // TODO - purposes
-        return registerApiModel.readinessCheck();
-    }
-
 }
