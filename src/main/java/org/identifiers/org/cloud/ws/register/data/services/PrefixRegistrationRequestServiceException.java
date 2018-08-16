0944584ff0fb177c09a543147ad8d9bcc4d6e3f5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-16 14:27:59 +0100

    PrefixRegistrationRequestServiceException

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/data/services/PrefixRegistrationRequestServiceException.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.register.data.services;
+
+/**
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.data.services
+ * Timestamp: 2018-08-16 14:27
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PrefixRegistrationRequestServiceException extends RuntimeException {
+    public PrefixRegistrationRequestServiceException(String message) {
+        super(message);
+    }
+}
