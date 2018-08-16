95957af5a10c5f7568d57794fd312c2161b08b89
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-16 13:30:08 +0100

    placeholder for PrefixRegistrationRequest configuration broker

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/data/configuration/PrefixRegistrationRequestConfig.java
@@ -0,0 +1,15 @@
+package org.identifiers.org.cloud.ws.register.data.configuration;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.data.configuration
+ * Timestamp: 2018-08-16 13:29
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class PrefixRegistrationRequestConfig {
+}
