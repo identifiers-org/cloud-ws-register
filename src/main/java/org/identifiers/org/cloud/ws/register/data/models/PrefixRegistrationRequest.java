aea4f4c2f8b32df7332070430ca2acc8ab1b3b91
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-16 12:03:08 +0100

    PrefixRegistrationRequest, model placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/data/models/PrefixRegistrationRequest.java
@@ -0,0 +1,14 @@
+package org.identifiers.org.cloud.ws.register.data.models;
+
+import java.io.Serializable;
+
+/**
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.data.models
+ * Timestamp: 2018-08-16 12:02
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class PrefixRegistrationRequest implements Serializable {
+}
