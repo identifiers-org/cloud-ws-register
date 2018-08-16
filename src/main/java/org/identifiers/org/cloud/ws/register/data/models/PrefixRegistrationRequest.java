aaed7fbca216259006518d28d7cf34382c5ba4c5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-16 13:39:24 +0100

    set default values

--- src/main/java/org/identifiers/org/cloud/ws/register/data/models/PrefixRegistrationRequest.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/data/models/PrefixRegistrationRequest.java
@@ -16,16 +16,16 @@ import java.io.Serializable;
  */
 @RedisHash("RegistryPrefixRegistrationRequest")
 public class PrefixRegistrationRequest implements Serializable {
-    private String name;
-    private String description;
-    private String homePage;
-    private String organization;
-    private String preferredPrefix;
-    private String resourceAccessRule;
-    private String exampleIdentifier;
-    private String regexPattern;
-    private String[] references;
-    private String additionalInformation;
+    private String name = "";
+    private String description = "";
+    private String homePage = "";
+    private String organization = "";
+    private String preferredPrefix = "";
+    private String resourceAccessRule = "";
+    private String exampleIdentifier = "";
+    private String regexPattern = "";
+    private String[] references = {""};
+    private String additionalInformation = "";
     private Requester requester;
     // Management
     private String token = "";
