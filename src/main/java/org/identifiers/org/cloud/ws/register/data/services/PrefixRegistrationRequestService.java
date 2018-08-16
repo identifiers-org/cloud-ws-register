194ed4d067c79128a6803b7a20593bde72ef7529
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-16 14:24:55 +0100

    PrefixRegistrationRequestService, findByPreferredPrefix() placeholder

--- src/main/java/org/identifiers/org/cloud/ws/register/data/services/PrefixRegistrationRequestService.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/data/services/PrefixRegistrationRequestService.java
@@ -1,10 +1,13 @@
 package org.identifiers.org.cloud.ws.register.data.services;
 
+import org.identifiers.org.cloud.ws.register.data.models.PrefixRegistrationRequest;
 import org.identifiers.org.cloud.ws.register.data.repositories.PrefixRegistrationRequestRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Project: register
@@ -16,9 +19,13 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class PrefixRegistrationRequestService {
-    private static final Logger logger = LoggerFactory.getLogger(PrefixRegistrationRequestService.class);
+    private static final Logger logger = LoggerFactory.getLogger(PrefixRegistrationRequest.class);
 
     @Autowired
     private PrefixRegistrationRequestRepository repository;
+
+    public List<PrefixRegistrationRequest> findByPreferredPrefix(String preferredPrefix) {
+        // TODO
+    }
     // TODO
 }
