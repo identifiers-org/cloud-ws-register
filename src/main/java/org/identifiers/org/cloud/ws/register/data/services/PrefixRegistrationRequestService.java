9100435826d5faf227c7bb87284bbed020d2a620
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-16 14:21:40 +0100

    PrefixRegistrationRequestService, TODO

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/data/services/PrefixRegistrationRequestService.java
@@ -0,0 +1,16 @@
+package org.identifiers.org.cloud.ws.register.data.services;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.data.services
+ * Timestamp: 2018-08-16 14:21
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+@Component
+public class PrefixRegistrationRequestService {
+    // TODO
+}
