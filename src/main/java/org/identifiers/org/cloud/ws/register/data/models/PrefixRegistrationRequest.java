ac65942782138b42e8d338fd94cf542ef58ec365
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-16 14:06:55 +0100

    getters and setters

--- src/main/java/org/identifiers/org/cloud/ws/register/data/models/PrefixRegistrationRequest.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/data/models/PrefixRegistrationRequest.java
@@ -46,5 +46,129 @@ public class PrefixRegistrationRequest implements Serializable {
         return this;
     }
 
+    public String getId() {
+        return id;
+    }
 
+    public PrefixRegistrationRequest setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PrefixRegistrationRequest setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public PrefixRegistrationRequest setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public PrefixRegistrationRequest setHomePage(String homePage) {
+        this.homePage = homePage;
+        return this;
+    }
+
+    public String getOrganization() {
+        return organization;
+    }
+
+    public PrefixRegistrationRequest setOrganization(String organization) {
+        this.organization = organization;
+        return this;
+    }
+
+    public String getPreferredPrefix() {
+        return preferredPrefix;
+    }
+
+    public PrefixRegistrationRequest setPreferredPrefix(String preferredPrefix) {
+        this.preferredPrefix = preferredPrefix;
+        return this;
+    }
+
+    public String getResourceAccessRule() {
+        return resourceAccessRule;
+    }
+
+    public PrefixRegistrationRequest setResourceAccessRule(String resourceAccessRule) {
+        this.resourceAccessRule = resourceAccessRule;
+        return this;
+    }
+
+    public String getExampleIdentifier() {
+        return exampleIdentifier;
+    }
+
+    public PrefixRegistrationRequest setExampleIdentifier(String exampleIdentifier) {
+        this.exampleIdentifier = exampleIdentifier;
+        return this;
+    }
+
+    public String getRegexPattern() {
+        return regexPattern;
+    }
+
+    public PrefixRegistrationRequest setRegexPattern(String regexPattern) {
+        this.regexPattern = regexPattern;
+        return this;
+    }
+
+    public String[] getReferences() {
+        return references;
+    }
+
+    public PrefixRegistrationRequest setReferences(String[] references) {
+        this.references = references;
+        return this;
+    }
+
+    public String getAdditionalInformation() {
+        return additionalInformation;
+    }
+
+    public PrefixRegistrationRequest setAdditionalInformation(String additionalInformation) {
+        this.additionalInformation = additionalInformation;
+        return this;
+    }
+
+    public Requester getRequester() {
+        return requester;
+    }
+
+    public PrefixRegistrationRequest setRequester(Requester requester) {
+        this.requester = requester;
+        return this;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public PrefixRegistrationRequest setToken(String token) {
+        this.token = token;
+        return this;
+    }
+
+    public Long getTimeToLeave() {
+        return timeToLeave;
+    }
+
+    public PrefixRegistrationRequest setTimeToLeave(Long timeToLeave) {
+        this.timeToLeave = timeToLeave;
+        return this;
+    }
 }
