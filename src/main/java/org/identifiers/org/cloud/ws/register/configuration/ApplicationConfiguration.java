52ad8773a0ec9088ee12c4a16d42800c8c1fb5d2
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-16 12:17:57 +0100

    jedis dependency

--- src/main/java/org/identifiers/org/cloud/ws/register/configuration/ApplicationConfiguration.java
+++ src/main/java/org/identifiers/org/cloud/ws/register/configuration/ApplicationConfiguration.java
@@ -1,8 +1,12 @@
 package org.identifiers.org.cloud.ws.register.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 /**
@@ -21,4 +25,18 @@ public class ApplicationConfiguration {
 
     @Value("${spring.redis.host}")
     private String redisHost;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost,
+                redisPort);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
 }
