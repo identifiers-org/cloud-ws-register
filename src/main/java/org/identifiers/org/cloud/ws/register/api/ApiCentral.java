5d4c7b5e41a87c37b866aa4b7fa8a4505bad1b72
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-17 16:42:17 +0100

    ApiCentral for api version control

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/api/ApiCentral.java
@@ -0,0 +1,14 @@
+package org.identifiers.org.cloud.ws.register.api;
+
+/**
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.api
+ * Timestamp: 2018-08-17 16:41
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ApiCentral {
+    // Current API Version flag
+    public static final String apiVersion = "1.0";
+}
