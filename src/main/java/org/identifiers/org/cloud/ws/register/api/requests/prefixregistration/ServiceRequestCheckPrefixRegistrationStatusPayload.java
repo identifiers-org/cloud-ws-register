f879d00d59d1908b53a9f806013f7311a3f34f3c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-17 15:01:17 +0100

    ServiceRequestCheckPrefixRegistrationStatusPayload payload placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/api/requests/prefixregistration/ServiceRequestCheckPrefixRegistrationStatusPayload.java
@@ -0,0 +1,13 @@
+package org.identifiers.org.cloud.ws.register.api.requests.prefixregistration;
+
+/**
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.api.requests.prefixregistration
+ * Timestamp: 2018-08-17 15:01
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceRequestCheckPrefixRegistrationStatusPayload {
+
+}
