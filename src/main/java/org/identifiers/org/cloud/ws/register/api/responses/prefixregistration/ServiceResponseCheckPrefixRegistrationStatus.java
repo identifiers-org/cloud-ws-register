f95c38783ae843331503c7c0f69d432f19cbcfb1
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-08-17 15:04:32 +0100

    ServiceResponseCheckPrefixRegistrationStatus service response placeholder

---
+++ src/main/java/org/identifiers/org/cloud/ws/register/api/responses/prefixregistration/ServiceResponseCheckPrefixRegistrationStatus.java
@@ -0,0 +1,12 @@
+package org.identifiers.org.cloud.ws.register.api.responses.prefixregistration;
+
+/**
+ * Project: register
+ * Package: org.identifiers.org.cloud.ws.register.api.responses.prefixregistration
+ * Timestamp: 2018-08-17 15:04
+ *
+ * @author Manuel Bernal Llinares <mbdebian@gmail.com>
+ * ---
+ */
+public class ServiceResponseCheckPrefixRegistrationStatus {
+}
